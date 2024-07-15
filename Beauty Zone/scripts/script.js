eab2a674fdb548c6b15fc02a03ee5d3f60f45e77
Psihys <bogdan.s.0474@gmail.com>
2024-07-15 22:05:59 +0200

    s

--- Beauty Zone/scripts/script.js
+++ Beauty Zone/scripts/script.js
@@ -12,4 +12,18 @@
                 backdrop.classList.add('hidden');
                  backdrop.classList.remove('backdrop')
             });
+ });
+        
+  document.addEventListener('DOMContentLoaded', function() {
+            const burgerButton = document.getElementById("burger_button");
+            const closeButton = document.querySelector('.close-burger-button');
+            const menu = document.getElementById("mobile-menu")
+
+     burgerButton.addEventListener('click', function () {
+         menu.classList.add('is-open')
+            });
+
+            closeButton.addEventListener('click', function() {
+                menu.classList.remove('is-open');
+            });
         });
