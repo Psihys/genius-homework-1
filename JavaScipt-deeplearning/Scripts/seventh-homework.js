b0bb30364017b74e4742c31068ceaefee2b43910
Psihys <bogdan.s.0474@gmail.com>
2024-08-06 18:19:54 +0200

    s

---
+++ JavaScipt-deeplearning/Scripts/seventh-homework.js
@@ -0,0 +1,109 @@
+// class Circle{
+//     constructor(selector) {
+//         this.$el = document.querySelector(selector)
+//     }
+
+//     hide() {
+//         this.$el.style.display = "none"
+//     }
+
+//     show() {
+//          this.$el.style.display = "block"
+//     }
+
+// }
+
+// class CircleItem extends Circle {
+//     constructor(options) {
+//         super(options.selector);
+//         this.$el.style.width = options.size + "px";
+//         this.$el.style.height = options.size + "px";
+//         this.$el.style.borderRadius = '50%';
+//          this.$el.style.background = options.color;
+//     }
+// }
+
+// const CircleRed = new CircleItem({
+//     selector: "#circleRed",
+//     color: "red",
+//     size:50,
+// });
+
+// const CircleBlue = new CircleItem({
+//     selector: "#circleBlue",
+//     color: "blue",
+//     size:80,
+// });
+
+// const CircleGreen = new CircleItem({
+//     selector: "#circleGreen",
+//     color: "green",
+//     size:110,
+// });
+
+// CircleRed.hide()
+// CircleGreen.hide()
+// CircleRed.show()
+// CircleGreen.show()
+
+
+// function Car (name,color){
+//     this.name = name;
+//     this.color = color;
+
+// }
+
+// Car.prototype.message = function () {
+//     console.log(`Mark is: ${this.name} and color is ${this.color}`);
+// }
+
+// Car.prototype.start = function () {
+//     console.log(`${this.name} is ready`)
+// }
+
+// const Audi = new Car("Audi", "Black")
+// const BMW = new Car("BMW", "White")
+
+// Audi.message()
+// BMW.start()
+
+// let message = {
+//     messageHello() {
+//         console.log(`Hello ${this.name}`);
+//     },
+
+//     messageBye() {
+//           console.log(`Bye ${this.name}`);
+//     },
+// }
+
+// class User {
+//     constructor(name) {
+//         this.name = name;
+//     }
+// }
+
+// Object.assign(User.prototype, message)
+ 
+// let Ivan = new User("Ivan");
+// Ivan.messageHello();
+
+let number = prompt("Write number");
+
+function show(number) {
+    for (let i = 2; i <= number; i++) {
+        let isPrime = true;
+        for (let j = 2; j < i; j++) {
+            if (i % j == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            console.log(i);
+        }
+    }
+}
+
+show(number);
+
--- JavaScipt-deeplearning/Scripts/sixth-homework.js
+++ JavaScipt-deeplearning/Scripts/sixth-homework.js
@@ -1,10 +1,148 @@
-const sum = 20000;
-const month = 12;
-const p = 1000;
+// const sum = 20000;
+// const month = 12;
+// const p = 1000;
 
-const credit = (sum, month, p) =>{
-    let total = sum + p * month;
-    console.log(total)
-}
+// const credit = (sum, month, p) =>{
+//     let total = sum + p * month;
+//     console.log(total)
+// }
 
-console.log(credit(sum, month, p))
+// console.log(credit(sum, month, p))
+
+
+// class Bank {
+//     static type = 'bank';
+//     constructor(options) {
+//         this.summ = options.summ;
+//         this.month = options.month;
+//         this.p = options.p;
+//     }
+//     credit() {
+//         return  console.log( "Personal info " + this.summ + this.month * this.p)
+//     }
+// }
+
+// const userBank = new Bank({
+//     summ: 50000,
+//     month: 12,
+//     p: 500,
+// })
+
+// userBank.credit()
+
+
+// class NewBank extends Bank {
+//     constructor(options) {
+//         super(options)
+//         this.card = options.card;
+//     }
+//     credit() {
+//         super.credit();
+//         return console.log("i am aval");
+//     }
+// }
+
+// const aval = new NewBank({
+//     summ: 50000,
+//     month: 12,
+//     p: 500,
+//     card: true,
+// })
+
+// console.log(aval.credit())
+
+
+// class User {
+//     constructor(props) {
+//         this.name = props.name;
+
+//     }
+//     firstName = "";
+//     lastName = "";
+//     age = "";
+//     city = "";
+
+//     set name(newName) {
+//         const nameRow = newName.split(" ");
+//         this.firstName = nameRow[0];
+//         this.lastName = nameRow[1];
+
+//     }
+
+//     get name() {
+//         return `First name is: ${this.firstName} Last name is: ${this.lastName}`;
+//     }
+// }
+
+// const Bohdan = new User({
+//     name: "Bohdan Shevchenko",
+// })
+
+// const Dasha = new User({
+//     name: "Dasha Shevchenko",
+// })
+
+// console.log(Bohdan)
+// console.log(Dasha)
+
+
+// const a = {
+//     x: 1,
+//     y: 2,
+    
+// };
+// const b = Object.create(a);
+
+// console.log(b)
+// console.log(b.x)
+// console.log(b.y)
+// console.log(b.hasOwnProperty("x"))
+
+
+// const Manager = function (name, sales) {
+//     this.name = name;
+//     this.sales = sales;
+
+//     // this.sale = function(){
+//     //     this.sales += 1;
+//     // };
+
+// };
+
+// const Ann = new Manager("Ann", 5);
+// const ivan = new Manager("Ivan", 10)
+
+
+// Manager.prototype.sale = function () {
+//     this.sales += 1;
+// }
+
+// console.log(ivan.sales);
+// ivan.sale();
+// console.log(ivan.sales)
+// console.log(ivan)
+
+// console.dir(Manager)
+
+// class coffeeMachine {
+//     _water = 0;
+//     #waterLimit = 500;
+
+//     constructor(power) {
+//         this.power = power;
+//     }
+
+//     set waterAmount(value) {
+//         if (value < 0) {
+//             value = 0;
+//         }
+//         this._water = value;
+//     }
+// }
+
+// let coffeeMachineNew = new coffeeMachine(100);
+
+// coffeeMachineNew.waterAmount = 20;
+
+// console.log(coffeeMachineNew)
+
