eaa831b36f215c4a0c6091b43b82c5fcae407b93
Psihys <bogdan.s.0474@gmail.com>
2024-08-15 20:16:48 +0200

    і

---
+++ JavaScipt-deeplearning/Scripts/12-homework/less 11/index.js
@@ -0,0 +1,15 @@
+// import isURL from "validator/lib/isURL";
+// import toDate from "validator/lib/toDate";
+// import { name } from "./module"
+// import "./style.css"
+// console.log("Your name is "  + name);
+
+// var moment = require("moment");
+
+// console.log(toDate('01.01.2005'));
+
+// console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+// console.log(moment().format('dddd'))
+// console.log(moment().format("MMM Do YY"))
+// console.log(moment().format('YYYY [escaped] YYYY'))
+// console.log(moment().format())
